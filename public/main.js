d8ef48c17d234285619d2c373d9f690cb6516205
Tiffany Lam <tlam5@sbcglobal.net>
2017-04-03 15:13:19 -0700

    Include fetch request with replacement quote

--- public/main.js
+++ public/main.js
@@ -1,5 +1,12 @@
 var update = document.getElementById('update');
 
 update.addEventListener('click', function() {
-  
+  fetch('quotes', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': 'Beach Weather',
+      'quote': "Don't you know you're just a heartbreaker."
+    })
+  });
 })
