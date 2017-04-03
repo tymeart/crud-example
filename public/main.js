9449c0d3832cf0ae3c9d390318116aab070e255f
Tiffany Lam <tlam5@sbcglobal.net>
2017-04-03 15:53:02 -0700

    Handle result from fetch request

--- public/main.js
+++ public/main.js
@@ -8,5 +8,14 @@ update.addEventListener('click', function() {
       'name': 'Beach Weather',
       'quote': "Don't you know you're just a heartbreaker."
     })
+  })
+  .then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(data => {
+    console.log(data);
+    window.location.reload(true);
   });
 })
